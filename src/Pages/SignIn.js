947c0f2c72ccc77f1462d2c0e5858ec0dcd4d61c
Flicter Alexandru <flicteralexandru@gmail.com>
2021-11-23 11:09:09 +0200

    Added preview

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,14 +1,33 @@
-import React,{useContext} from "react";
+import React,{useContext,useState,useEffect} from "react";
 import './SignIn.css'
 import logo from '../Components/Images/instagramLogo.png'
 import {Context} from '../Context'
 import {auth} from '../firebase'
 import {signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+import iphoneImage from '../Components/Images/iphoneimage.jpg'
+import showImages from "../showImages";
 
 
 function SignIn(){
+    const [index,setIndex]=useState(0)
     const {setUser}=useContext(Context)
     const provider = new GoogleAuthProvider()
+
+    function nextSlide(){
+        if(index<showImages.length-1){
+            setIndex(index+1)
+        }
+        else{
+           setIndex(0)
+        }
+    }
+
+    useEffect(()=>{
+        const intervalId = setInterval(nextSlide,5000)
+
+        return ()=>clearInterval(intervalId)
+    },[index])
+        
 
     function handleClick(){
         signInWithPopup(auth,provider)
@@ -21,8 +40,18 @@ function SignIn(){
     }
     return( 
     <div className='signInContainer'>
+        <div className='iphoneContainer'>
+            <img src={iphoneImage} className='iphonePic'/>
+            {showImages.map((item,ind)=>(
+                <img key={ind} src={item.image} alt={'Preview'} className={index===ind?'iphoneContent':'iphoneContentNoDisplay'}/>
+                )
+            )}
+        </div>
+        
+        <div className='signInInner'>
         <img src={logo} className='signInLogo' alt='Instagram Logo'/>
-        <button className='signInButton' onClick={handleClick}>Sign In</button>    
+        <button className='signInButton' onClick={handleClick}>Sign In</button>  
+        </div>
     </div>
     )
 }
---
+++ src/showImages.js
@@ -0,0 +1,43 @@
+import iphone1 from './Components/Images/iphone1.png'
+import iphone2 from './Components/Images/iphone2.png'
+import iphone3 from './Components/Images/iphone3.png'
+import iphone4 from './Components/Images/iphone4.png'
+import iphone5 from './Components/Images/iphone5.png'
+import iphone6 from './Components/Images/iphone6.png'
+import iphone7 from './Components/Images/iphone7.png'
+import iphone8 from './Components/Images/iphone8.png'
+import iphone9 from './Components/Images/iphone9.png'
+import iphone10 from './Components/Images/iphone10.png'
+
+export default [
+    {
+    image:iphone1
+    },
+    {
+    image:iphone2
+    },
+    {
+    image:iphone3
+    },
+    {
+    image:iphone4
+    },
+    {
+    image:iphone5
+    },
+    {
+    image:iphone6
+    },
+    {
+    image:iphone7
+    },
+    {
+    image:iphone8
+    },
+    {
+    image:iphone9
+    },
+    {
+    image:iphone10
+    }
+]
